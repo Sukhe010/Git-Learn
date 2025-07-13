c0296b8d1aa15e787e2a7afb4f67f8f47f14fdcf
Sukhe <sukhenegi01@gmail.com>
2025-07-13 23:18:03 +0530

    Added msg to the file

--- hello.java
+++ hello.java
@@ -1,3 +1,6 @@
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("Now i know how to push local dirctory to the remote directory like: My system to GitHub ");
+    }
 
 }
