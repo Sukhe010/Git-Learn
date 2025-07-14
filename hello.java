44f6e5e4d79e2f7a204529b88f44065b302280f8
Sukhe <sukhenegi01@gmail.com>
2025-07-14 22:56:49 +0530

    Day two of learning git

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Now i know how to push local dirctory to the remote directory like: My system to GitHub ");
+        System.out.println("Day2: of learning Git.");
     }
 
 }
