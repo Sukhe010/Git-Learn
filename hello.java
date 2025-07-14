e9150c9a928dec5c9bcdeb6791923ef341260c8d
Sukhe <sukhenegi01@gmail.com>
2025-07-14 23:07:39 +0530

    Created a new branch called -> feature1

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Now i know how to push local dirctory to the remote directory like: My system to GitHub ");
         System.out.println("Day2: of learning Git.");
+        //Created new branch
+        System.out.println("Created new branch called -> feature1.");
     }
 
 }
